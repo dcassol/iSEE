76ba5b7a7daee2292e9a09b4eb8f3f66e6d5ad5e
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2019-04-09 22:41:45 +0100

    fix unit test (see 18d6cef6d3d80ada89c097dca88a459c81488c35)

--- tests/testthat/test_custom.R
+++ tests/testthat/test_custom.R
@@ -14,7 +14,7 @@ CUSTOM_DATA <- function(se, rows, columns, colour_by=NULL, scale_columns=TRUE) {
     }
 
     scale_columns <- as.logical(scale_columns)
-    kept <- runPCA(kept, feature_set=rows, scale_columns=scale_columns)
+    kept <- runPCA(kept, feature_set=rows, scale_features=scale_columns)
 	plotPCA(kept, colour_by=colour_by)
 }
 
